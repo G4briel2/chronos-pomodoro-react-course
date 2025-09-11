a60c2f91ead4873eab779bd30ea1f68a2fee83b6
Gabriel de Almeida Martins <gabriel_241@outlook.com.br>
2025-09-10 22:42:09 -0300

    Criação de css global

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
+import './styles/theme.css'
+import './styles/global.css'
+
 export function App() {
     return (
         <>
             <h1>Hello World!</h1>
-            <h1>Nothing special</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas eligendi veniam nesciunt delectus fuga dolore, doloribus, obcaecati assumenda magnam labore doloremque maiores atque necessitatibus nisi similique quos! Laborum, consectetur maiores.</p>
         </>
     )
 }
