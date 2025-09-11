77dca2100b6e53807d9d1f81d8c7e7f7ea44f1f2
Gabriel de Almeida Martins <gabriel_241@outlook.com.br>
2025-09-10 23:02:08 -0300

    Criação de css específico

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
+import { Heading } from './components/Heading'
+
 import './styles/theme.css'
 import './styles/global.css'
 
 export function App() {
     return (
         <>
-            <h1>Hello World!</h1>
+            <Heading />
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas eligendi veniam nesciunt delectus fuga dolore, doloribus, obcaecati assumenda magnam labore doloremque maiores atque necessitatibus nisi similique quos! Laborum, consectetur maiores.</p>
         </>
     )
